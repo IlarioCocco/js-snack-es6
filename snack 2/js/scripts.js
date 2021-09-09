510e307d5078e8cb4a093f75cea058786aaca411
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-09 18:34:03 +0200

    commit

--- snack 2/js/scripts.js
+++ snack 2/js/scripts.js
@@ -32,25 +32,29 @@ const squadraDiCalcio = [
     puntiFatti: 0,
     falliSubiti:0,
 },
+
 ];
 
+
+let falliSquadre = squadraDiCalcio[0];
+
 for (let i = 0; i < squadraDiCalcio.length; i++ ){
-    console.log(squadraDiCalcio[i].puntiFatti);
-    console.log(squadraDiCalcio[i].falliSubiti);
+    // console.log(squadraDiCalcio[i].puntiFatti);
+    // console.log(squadraDiCalcio[i].falliSubiti);
     squadraDiCalcio[i].puntiFatti = rand(1, 100);  //nome della funzione ()
     squadraDiCalcio[i].falliSubiti = rand(1, 100); //nome della funzione ()
 }
 console.log(squadraDiCalcio);
 
 
-// Generare numeri random al posto degli 0 nelle proprietà:
+// Generare numeri random al posto degli 0 nelle proprietà (puntiFatti & falliSubiti tramite una function:
 function rand(num1, num2) {
     return (Math.floor(Math.random() * num2) + num1);
 }
-console.log(rand(1, 100));
+// console.log(rand(1, 100));
 
 
 
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-const {nome, falliSubiti} = squadraDiCalcio;
-console.log(`nomi squadre: ${nome} e falli subiti: ${falliSubiti}`);
+const { nome, falliSubiti } = falliSquadre;
+console.log(`nome squadra: ${nome} e falli subiti: ${falliSubiti}`);
