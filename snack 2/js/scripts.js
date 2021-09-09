3c4245e1846b642563d7e1aab7641380e26ec24a
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-09 17:13:16 +0200

    commit genera numeri random

--- snack 2/js/scripts.js
+++ snack 2/js/scripts.js
@@ -2,8 +2,7 @@
 
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà:
-// Punti fatti e falli subiti.
+// Generare numeri random al posto degli 0 nelle proprietà Punti fatti e falli subiti.
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 // 1 Creare un array di oggetti di squadre di calcio
@@ -34,3 +33,10 @@ let squadraDiCalcio = [
     falliSubiti:0,
 },
 ];
+
+
+
+// Generare numeri random al posto degli 0 nelle proprietà:
+var numeroRandom = (Math.floor(Math.random() * 100) + 1);
+console.log("numeri Random:" + numeroRandom)
+
