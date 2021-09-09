e1e67f2fe6433c6f4833afc1c664b00d7eb71555
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-09 16:48:01 +0200

    commit

--- snack 2/js/scripts.js
+++ snack 2/js/scripts.js
@@ -7,28 +7,30 @@
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 // 1 Creare un array di oggetti di squadre di calcio
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 let squadraDiCalcio = [
 {   
     nome: "Roma",
-    puntiFatti: 50,
-    falliSubiti:15,
+    puntiFatti: 0,
+    falliSubiti:0,
 },
 
 {
     nome: "MIlan",
-    puntiFatti: 45,
-    falliSubiti:14,
+    puntiFatti: 0,
+    falliSubiti:0,
 },
 
 {
     nome: "Inter",
-    puntiFatti: 35,
-    falliSubiti:13,
+    puntiFatti: 0,
+    falliSubiti:0,
 },
 
 {
     nome: "Juve",
-    puntiFatti: 25,
-    falliSubiti:12,
+    puntiFatti: 0,
+    falliSubiti:0,
 },
 ];
