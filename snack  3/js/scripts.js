ffb6549045493cd7e39391c7cc5e2c6466dd8974
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-12 15:42:39 +0200

    destrutturazione (Destructuring assignment) (commit)

--- snack  3/js/scripts.js
+++ snack  3/js/scripts.js
@@ -43,17 +43,22 @@
 const array_primario = ["andrea", "barbara", "carlo", "dario", "emanuele"];
 console.log(array_primario);
 
-// const possibileSoluzione = array_primario.filter(
-//     (elm, i) => {
-//         return i >= 1 && i <= 3 ;
-//     }
-// ); console.log(`gli estremi sono compresi tra 1 & 3 : ${possibileSoluzione}`)
+const possibileSoluzione = array_primario.filter(
+    (elm, i) => {
+        return i >= 1 && i <= 3 ;
+    }
+); console.log(`gli estremi sono compresi tra 1 & 3 : ${possibileSoluzione}`)
 
 
 
 
-const possibileSoluzione = array_primario.filter((i) => { return i >= 1 && i <= 3;}); 
+
+// destrutturazione(Destructuring assignment)
+
+const possibileSoluzione = array_primario.filter((i) => { return i >= 1 && i <= 3; }); 
 console.log(`gli estremi sono compresi tra 1 & 3 : ${possibileSoluzione}`)
+
+
 
 
 
@@ -61,16 +66,16 @@ console.log(`gli estremi sono compresi tra 1 & 3 : ${possibileSoluzione}`)
 
 // FOR EACH-----------------------
 
-// const array_primario = ["a", "b", "c", "d", "e"];
-// console.log(array_primario);
+const array_primario = ["a", "b", "c", "d", "e"];
+console.log(array_primario);
 
-// array_primario.forEach((element, index) => {
-//     // console.log(element, index);
-//     if (index >= 1 && index <= 3 ){
+array_primario.forEach((element, index) => {
+    // console.log(element, index);
+    if (index >= 1 && index <= 3 ){
        
-//         console.log(element);
-//     }
-// }); 
+        console.log(element);
+    }
+}); 
 
 
 
